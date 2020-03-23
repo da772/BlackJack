9b3394d7451a02755bd802a80b49bd626fb5db51
zhangted <tedcbook@gmail.com>
2020-03-23 04:42:43 -0400

    game logic tweak
    -if dealer has 21 as initial hand, skip to end state
    -showing the double button now also requires player to have enough money

--- src/application/Round.java
+++ src/application/Round.java
@@ -82,6 +82,9 @@ public class Round {
 				state = RoundState.Playing;
 				// Deal cards
 				this.dealCards(dealer, players, deck);
+				if(dealer.getHand().getTotal() == 21) {//dealer has 21 as init hand, immediately go to end state
+					state = RoundState.Ending;
+				}
 				// Reset player increment
 				playerTurn = 0;
 			}
